973500d0669426e0b3adf0e7f5834ab9c3926d11
Gustavo-mts <gm357191@gmail.com>
2024-09-16 23:03:39 -0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(port);
 }
 
 bootstrap();
